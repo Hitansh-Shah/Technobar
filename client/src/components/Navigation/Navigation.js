baa6c61d4745108d520ece78596bfdd8534c3c78
Hitansh Shah <shah.hitanshsanjay.mat20@itbhu.ac.in>
2021-10-04 19:27:59 +0530

    change main app icon
    Signed-off-by: Hitansh Shah <shah.hitanshsanjay.mat20@itbhu.ac.in>

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from 'react';
-import {Navbar, Container, Nav, Button } from 'react-bootstrap';
+import {Navbar, Container, Nav, Button, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Navigation.css';
+import Icon from './id_card.png'
 
 function Navigation(props) {
     
@@ -19,7 +20,7 @@ function Navigation(props) {
     return (
         <Navbar bg="dark" variant="dark" expand="lg" className="justify-content-center mynavbar">
             <Container className="margin">
-                <Navbar.Brand as={Link} to="/" className="technobar">Technobar</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/" className="technobar"><div style={{display:"flex", justifyContent:"center"}}><Image src={Icon} width="50"/><div style={{display:"flex", alignItems:"center"}}><h3 style={{ marginLeft:"10px", marginBottom:"0px"}}>Technobar</h3></div></div></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
                     {props.isLoggedIn ?
